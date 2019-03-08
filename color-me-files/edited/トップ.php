e2dedec4a41b7044cefc889c7bb78ead554ae782
Kumazaki Shinnosuke <s.kumazaki@mikke.co.jp>
2019-03-08 15:47:03 +0900

    本番環境の更新ファイルを追加

---
+++ color-me-files/edited/トップ.php
@@ -0,0 +1,226 @@
+<article id="index">
+  <section id="main">
+    <div id="mainVisual">
+      <div id="scrollDown" class="scroll_button">
+        <a href="javascript:void(0)" jump="#topConcept"><span>SCROLL</span></a>
+      </div>
+    </div>
+  </section><!-- main -->
+  <section id="topConcept">
+    <div class="ttl_section center">
+      <h2 class="ttl_ja">完熟を超えた、果物を</h2>
+      <span class="ttl_en">BEYOND RIPE FRUITS</span>
+    </div>
+    <div class="img_wrap_sp"></div>
+    <div class="lead_txt">
+      <h3 class="ja">日本のプレミアム品種の果物をつかった、<span>「レア･ドライフルーツ」が誕生しました。</span></h3>
+      <span class="en">I used fruits from Japan's premium variety,<span>"Rare / dried fruit" was born.</span>
+    </div>
+    <div class="img_wrap"></div>
+    <div class="link_button01">
+      <a href="#aaaa"><span>詳しく知る<font>READ MORE</font></span></a>
+    </div>
+  </section><!-- topConcept -->
+  <section id="topRareDry">
+    <div class="inner">
+      <div class="flex_wrap">
+        <div class="flex_left">
+          <div class="ttl_section">
+            <h2 class="ttl_ja">完熟を超えた、果物を</h2>
+            <span class="ttl_en">BEYOND RIPE FRUITS</span>
+          </div>
+          <div class="spimg_wrap"></div>
+          <div class="txt">
+            <p class="ja"><span>日本のプレミアム品種を使った、レア・ドライフルーツ。</span><span>果物のみずみずしさを保ちながら、うまみを凝縮した、生の状態に近いドライフルーツです。完熟の状態で収穫し、製造過程で追熟しているので、生のフルーツよりも甘く、濃い味わいに仕上がっています。</span></p>
+            <p class="en">Rare / dried fruit using Japanese premium variety.It is a dried fruit close to raw state, condensing umami while keeping the freshness of fruits.Because it is harvested in ripe state and ripened in the manufacturing process,It is sweeter than raw fruit, finished in a rich taste.</p>
+          </div>
+          <div class="link_button01">
+            <a href="#aaaa"><span>詳しく知る<font>READ MORE</font></span></a>
+          </div>
+        </div><!-- flex_left -->
+        <div class="flex_right">
+          <span></span>
+        </div>
+      </div><!-- flex_set -->
+    </div>
+  </section><!-- topRareDry -->
+  <div id="topProduct">
+    <div class="inner">
+      <div class="ttl_section">
+        <h2 class="ttl_ja">季節の商品</h2>
+        <span class="ttl_en">PRODUCT LINEUP</span>
+      </div>
+    </div><!-- inner -->
+    <section id="kawanaka" class="prod_section">
+      <div class="productBg">
+        <a class="product_caret" href="#aaaa"><span class="caret"></span></a>
+        <div class="txt_wrapper right">
+          <div class="txt_block">
+            <span class="en">KAWANAKAJIMA PEACH</span>
+            <h3 class="ja">川中島白桃</h3>
+            <a class="prod_link" href="#aaaa"><span>VIEW MORE</span></a>
+          </div>
+          <div class="height_adjuster"></div>
+        </div>
+      </div>
+    </section>
+    <section id="pione" class="prod_section">
+      <div class="productBg">
+        <a class="product_caret" href="#aaaa"><span class="caret"></span></a>
+        <div class="txt_wrapper">
+          <div class="txt_block">
+            <span class="en">PIONE</span>
+            <h3 class="ja">ピオーネ</h3>
+            <a class="prod_link" href="#aaaa"><span>VIEW MORE</span></a>
+          </div>
+          <div class="height_adjuster"></div>
+        </div>
+      </div>
+    </section>
+    <section id="reiou" class="prod_section">
+      <div class="productBg">
+        <a class="product_caret" href="#aaaa"><span class="caret"></span></a>
+        <div class="txt_wrapper right">
+          <div class="txt_block">
+            <span class="en">REIOU</span>
+            <h3 class="ja">麗王</h3>
+            <a class="prod_link" href="#aaaa"><span>VIEW MORE</span></a>
+          </div>
+          <div class="height_adjuster"></div>
+        </div>
+      </div>
+    </section>
+    <section id="shine" class="prod_section">
+      <div class="productBg">
+        <a class="product_caret" href="#aaaa"><span class="caret"></span></a>
+        <div class="txt_wrapper">
+          <div class="txt_block">
+            <span class="en">SHINE MASCUT</span>
+            <h3 class="ja">シャインマスカット</h3>
+            <a class="prod_link" href="#aaaa"><span>VIEW MORE</span></a>
+          </div>
+          <div class="height_adjuster"></div>
+        </div>
+      </div>
+    </section>
+    <section id="kiyou" class="prod_section">
+      <div class="productBg">
+        <a class="product_caret" href="#aaaa"><span class="caret"></span></a>
+        <div class="txt_wrapper right">
+          <div class="txt_block">
+            <span class="en">KIYOU</span>
+            <h3 class="ja">貴陽</h3>
+            <a class="prod_link" href="#aaaa"><span>VIEW MORE</span></a>
+          </div>
+          <div class="height_adjuster"></div>
+        </div>
+      </div>
+    </section>
+    <div class="product_all">
+      <a href="#aaaa">
+        <span class="txt">商品一覧へ<font>VIEW PRODUCTS</font></span><span class="obj"></span>
+        <span class="txt_sp"><font>VIEW PRODUCTS</font><span>商品一覧へ</span></span>
+      </a>
+    </div>
+  </div><!-- topProduct -->
+  <section id="topOption">
+    <div class="ttl_section center">
+      <h2 class="ttl_ja">購入オプション</h2>
+      <span class="ttl_en">OPTIONS</span>
+    </div>
+    <div class="lead_txt">
+      <p><span>FRUITESTでは、記念日の贈り物向けのギフトセット・ラッピングサービスや</span><span>季節毎に旬の果物をお届けする定期便など、様々なオプションをご用意しております。</span></p>
+    </div>
+    <div class="flex_wrap">
+      <div class="block gift">
+        <span class="sp_ttl">ギフト商品一覧</span>
+        <a class="img_wrap" href="#aaa"></a>
+        <div class="txt_wrap">
+          <span class="ttl">ギフト商品一覧</span>
+          <div class="link_button02">
+            <a href="#aaaa"><span>詳しく知る</span></a>
+          </div>
+        </div>
+      </div>
+      <div class="block subscription">
+        <span class="sp_ttl">定期便について</span>
+        <a class="img_wrap" href="#aaa"></a>
+        <div class="txt_wrap">
+          <span class="ttl">定期便について</span>
+          <div class="link_button02">
+            <a href="#aaaa"><span>詳しく知る</span></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section><!-- topOption -->
+  <section id="JournalWrap">
+    <div class="inner">
+      <div class="ttl_section">
+        <h2 class="ttl_ja">FRUITESTについて<span>もっと知る。</span></h2>
+        <span class="ttl_en">JOURNAL</span>
+      </div>
+      <div class="journal_wrap">
+        <div id="slickSlider" class="slick-slider">
+          <div class="item_box slick-slide">
+            <a
+              class="img_wrap"
+              href="#aaaa"
+              style="background-image: url(http://fruitest.sakura.ne.jp/journal/files/img/index/journal01.jpg);"
+            >
+            </a>
+            <div class="txt_wrap">
+              <span class="date">10月<span>31日</span></span>
+              <a class="ttl" href="#aaaa">FRUITESTを作るにあたって</a>
+            </div>
+          </div>
+          <div class="item_box slick-slide">
+            <a
+              class="img_wrap"
+              href="#aaaa"
+              style="background-image: url(http://fruitest.sakura.ne.jp/journal/files/img/index/journal02.jpg);"
+            >
+            </a>
+            <div class="txt_wrap">
+              <span class="date">9月<span>4日</span></span>
+              <a class="ttl" href="#aaaa">”天空の茶畑”産ティーシードオイルについて</a>
+            </div>
+          </div>
+          <div class="item_box last slick-slide">
+            <a
+              class="img_wrap"
+              href="#aaaa"
+              style="background-image: url(http://fruitest.sakura.ne.jp/journal/files/img/index/journal03.jpg);"
+            >
+            </a>
+            <div class="txt_wrap">
+              <span class="date">7月<span>1日</span></span>
+              <a class="ttl" href="#aaaa">「Komons」の名前の由来は「江戸小紋」</a>
+            </div>
+          </div>
+        </div>
+        <div class="link_button01">
+          <a href="#aaaa"><span>ジャーナル一覧へ<font>VIEW JOURNAL</font></span></a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="topInstagram">
+    <div class="ttl_section center">
+      <h2 class="ttl_ja">#FRUITEST</h2>
+    </div>
+    <div id="instaFeed">
+      <ul>
+        <li><a href="#aaaa" style="background-image: url(http://fruitest.sakura.ne.jp/journal/files/img/index/insta01.jpg)"></a></li>
+        <li><a href="#aaaa" style="background-image: url(http://fruitest.sakura.ne.jp/journal/files/img/index/insta02.jpg)"></a></li>
+        <li><a href="#aaaa" style="background-image: url(http://fruitest.sakura.ne.jp/journal/files/img/index/insta03.jpg)"></a></li>
+        <li><a href="#aaaa" style="background-image: url(http://fruitest.sakura.ne.jp/journal/files/img/index/insta04.jpg)"></a></li>
+        <li><a href="#aaaa" style="background-image: url(http://fruitest.sakura.ne.jp/journal/files/img/index/insta05.jpg)"></a></li>
+        <li><a href="#aaaa" style="background-image: url(http://fruitest.sakura.ne.jp/journal/files/img/index/insta06.jpg)"></a></li>
+      </ul>
+      <div class="ig_link">
+        <a href="#aaaa"><span>@FRUITEST.JP</span></a>
+      </div>
+    </div>
+  </section>
+</article>
---
+++ color-me-files/edited/共通.php
@@ -0,0 +1,107 @@
+<header>
+  <div class="header_inner">
+    <div id="hummenu">
+      <a id="humButton"></a>
+    </div>
+    <div class="header_logo">
+      <a href="<{$home_url}>"></a>
+    </div>
+    <div class="header_nav">
+          <a class="nav_login" href="<{$view_myaccount_url}>">
+          <{if $members_login_flg != true}>
+            <span><font>ログイン</font></span>
+           <{else}>
+            <span><font>マイページ</font></span>
+         <{/if}>
+        </a>
+      <a class="nav_cart" href="<{$view_cart_url}>"><span><font>カート</font></span></a>
+    </div>
+  </div><!-- header_inner -->
+</header>
+<{include file = $file_name}>
+<footer>
+  <div class="footer_inner">
+    <div class="footer_upper">
+      <a class="footer_logo" href="#aaaa"></a>
+      <div class="sns_wrap">
+        <ul>
+          <li><a class="tw" href="#aaaa"></a></li>
+          <li><a class="fb" href="#aaaa"></a></li>
+          <li><a class="ig" href="#aaaa"></a></li>
+        </ul>
+      </div>
+    </div><!-- footer_upper -->
+    <div class="footer_middle">
+      <nav>
+        <ul>
+          <li><a href="#aaaa"><span>TOP</span>トップページへ</a></li>
+          <li><a href="#aaaa"><span>CONCEPT</span>コンセプト</a></li>
+          <li><a href="#aaaa"><span>PRODUCTS</span>商品一覧</a></li>
+          <li><a href="#aaaa"><span>GIFT</span>ギフト商品一覧</a></li>
+          <li><a href="#aaaa"><span>JOURNAL</span>ジャーナル</a></li>
+          <li><a href="#aaaa"><span>CONTACT</span>お問い合わせ</a></li>
+        </ul>
+      </nav>
+      <div class="guide">
+        <span class="ttl">ご利用ガイド</span>
+        <div class="guide_nav">
+          <a href="#aaaa">製品について</a>
+          <a href="#aaaa">ご購入に際して</a>
+          <a href="#aaaa">ギフトについて</a><br>
+          <a href="#aaaa">配送について</a>
+          <a href="#aaaa">キャンセルポリシー</a>
+        </div>
+      </div>
+    </div><!-- footer_middle -->
+    <div class="footer_lower">
+      <div class="lower_nav">
+        <nav>
+          <{if $members_login_flg != true}>
+          <a href="<{$view_myaccount_url}>">ログイン</a>
+           <{else}>
+          <a href="<{$view_myaccount_url}>">マイページ</a>
+          <a href="<{$members_login_url}>">ログアウト</a>
+         <{/if}>
+        <a href="<{$view_cart_url}>">カートを見る</a>
+        <a href="<{$privacy_url}>">プライバシーポリシー</a>
+        <br class="sp_reline">
+        <a href="#aaaa">FAQ</a>
+        <br class="pc_reline">
+        <a href="<{$sk_url}>">特定商取引法に基づく表記</a>
+        <br class="sp_reline">
+        <a href="https://unsungs-web.com/" target="_blank">運営会社</a>
+        <a href="<{$view_inq_url}>">お問い合わせ</a>
+        </ul>
+        <p class="copyright">&#169; 2018 Fruitest.</p>
+      </div>
+    </div>
+  </div>
+</footer>
+<div id="slideMenu">
+  <div id="menuBg"></div>
+  <div class="menu_wrap">
+    <div id="closeBtn"><a href="javascript:void(0);"></a></div>
+    <div class="menu_inner">
+      <div class="hum_navi">
+        <nav>
+        <ul>
+          <li><a href="#aaaa"><span>TOP</span><font>トップページ</font></a></li>
+          <li><a href="#aaaa"><span>CONCEPT</span><font>コンセプト</font></a></li>
+          <li><a href="#aaaa"><span>PRODUCTS</span><font>商品一覧</font></a></li>
+          <li><a href="#aaaa"><span>GIFT</span><font>ギフト商品一覧</font></a></li>
+          <li><a href="#aaaa"><span>GUIDE</span><font>ご利用ガイド</font></a></li>
+          <li><a href="#aaaa"><span>FAQ</span><font>よくある質問</font></a></li>
+          <li><a href="#aaaa"><span>CONTACT</span><font>お問い合わせ</font></a></li>
+        </ul>
+      </nav>
+      </div>
+      <div class="menu_sns">
+        <ul>
+          <li><a class="tw" href="#aaaa"></a></li>
+          <li><a class="fb" href="#aaaa"></a></li>
+          <li><a class="ig" href="#aaaa"></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div><!-- slideMenu -->
