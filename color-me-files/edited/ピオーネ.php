da766ce13340e6f8ffbc5f5f11545fc0e41a42c3
kakinumakana <kakinumakana@icloud.com>
2019-05-16 18:22:57 +0900

    ピオーネ商品詳細ページ
    ピオーネ商品ページのコンテンツの編集

---
+++ color-me-files/edited/ピオーネ.php
@@ -0,0 +1,111 @@
+<p id="leadTxt">
+糖度16度以上の強い甘みと、それに負けない爽やかな酸味。レア・ドライ製法で、甘みと酸味を究極のバランスで凝縮した逸品です。大玉ぶどうならではのジューシーさが楽しめる贅沢な味わいを、どうぞご堪能ください。
+</p>
+
+<section id="itemFeature">
+  <div class="inner">
+  <div class="flex_wrap reverse">
+    <div class="flex_img">
+      <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kiyou/detail01.jpg">
+    </div>
+    <div class="flex_txt">
+      <div class="comp-title">
+        <h2 class="ttl_ja">商品の特徴</h2>
+        <span class="ttl_en">FEATURES</span>
+      </div>
+      <div class="sp_img">
+        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kiyou/detail01.jpg">
+      </div>
+      <!--<h3 class="copy">
+        <span>収穫期間、わずか10日。</span>
+        <span>開発期間、20年。</span>
+      </h3>-->
+      <p class="txt">巨峰とマスカットの交配から生まれた大玉の黒ぶどう。<span>「ピオーネ」という言葉は、イタリア語で「開拓者」という意味をもちます。その名の通り、今では当たり前となった種なしぶどうの先駆者的な存在として、日本を代表する品種です。</span>
+      <span>やわらかくてみずみずしい果肉にくわえ、強い甘みとそれを支える酸味を併せ持つのが特徴です。</span></p>
+    </div>
+  </div><!-- flex_wrap -->
+  <div class="flex_wrap">
+    <div class="flex_img">
+      <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kiyou/detail02.jpg">
+    </div>
+    <div class="flex_txt">
+      <div class="comp-title">
+        <h2 class="ttl_ja">風味の特徴</h2>
+        <span class="ttl_en">FLAVOR</span>
+      </div>
+      <div class="sp_img">
+        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kiyou/detail02.jpg">
+      </div>
+      <p class="txt">大玉ぶどうのピオーネは、プチっと噛むと、みずみずしい果肉がじゅわっと口の中に広がります。<span>凝縮された濃い甘さと、それを引き立てる酸味の絶妙なバランスをお楽しみください。</span></p>
+      <div class="chart_wrap">
+        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kiyou/chart.png">
+      </div>
+    </div>
+  </div><!-- flex_wrap -->
+  <div class="flex_origin">
+    <div class="flex_img">
+      <div class="origin_img">
+        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kiyou/location.png">
+      </div>
+    </div>
+    <div class="flex_txt">
+      <div class="comp-title">
+        <h2 class="ttl_ja">原産地</h2>
+        <span class="ttl_en">PLACE OF ORIGIN</span>
+      </div>
+      <div class="sp_img">
+        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kiyou/location.png">
+      </div>
+      <div class="origin_upper">
+        <div class="origin_item">
+          <span class="title">産地</span>
+          <span class="content">山梨県韮崎市</span>
+        </div>
+        <div class="origin_item">
+          <span class="title">旬の時期</span>
+          <span class="content">8月下旬～10月初旬</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div><!-- inner -->
+</section><!-- itemFeature -->
+<section id="paringWrap">
+<div class="inner">
+  <div class="comp-title">
+    <h2 class="ttl_ja">オススメの食べ方</h2>
+    <span class="ttl_en">PARING SUGGESTION</span>
+  </div>
+  <div class="paring_wrap">
+    <span>フルーティで酸味のある、辛口の白ワイン</span>
+    <span>辛口の白ワイン</span>
+    <span>酸味のあるワイン</span>
+  </div>
+</div>
+</section>
+<section id="itemInfo">
+<div class="inner">
+  <div class="comp-title">
+    <h2 class="ttl_ja">商品情報</h2>
+    <span class="ttl_en">ITEM DETAIL</span>
+  </div>
+  <div class="info_wrap">
+    <div class="info_item">
+      <span class="title">原材料</span>
+      <span class="content">ぶどう（ピオーネ）)</span>
+    </div>
+    <div class="info_item">
+      <span class="title">産地</span>
+      <span class="content">山梨県韮崎市</span>
+    </div>
+    <div class="info_item">
+      <span class="title">内容量</span>
+      <span class="content">10個（10粒）</span>
+    </div>
+    <div class="info_item">
+      <span class="title">保存方法</span>
+      <span class="content">直射日光を避け、冷暗所に保存してください。<br>※賞味期限は製造から1年になります。商品の缶底をご確認ください。<br>※手作業で種抜きをしていますが、まれに種が残っている場合があります。<br>※開封後は密封し、お早めにお召し上がりください。<br>※開封後の保存は、冷蔵庫をおすすめします。<br>※商品の仕様などは予告なく変更になる場合があります。予めご了承ください。</span>
+    </div>
+  </div><!-- info_wrap -->
+</div><!-- inner-->
+</section><!-- itemInfo -->
