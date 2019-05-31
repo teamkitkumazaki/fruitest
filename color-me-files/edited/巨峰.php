bb6c212502b08a05c1adb8b07b94fffe8884288d
kakinumakana <kakinumakana@icloud.com>
2019-05-31 16:17:00 +0900

    シャインマスカット・甲州百目・アレキサンドリア画像編集

--- color-me-files/edited/巨峰.php
+++ color-me-files/edited/巨峰.php
@@ -6,7 +6,7 @@
   <div class="inner">
   <div class="flex_wrap reverse">
     <div class="flex_img">
-      <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kiyou/detail01.jpg">
+      <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kyohou/detail01.jpg">
     </div>
     <div class="flex_txt">
       <div class="comp-title">
@@ -14,7 +14,7 @@
         <span class="ttl_en">FEATURES</span>
       </div>
       <div class="sp_img">
-        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kiyou/detail01.jpg">
+        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kyohou/detail01.jpg">
       </div>
       <!--<h3 class="copy">
         <span>収穫期間、わずか10日。</span>
@@ -25,7 +25,7 @@
   </div><!-- flex_wrap -->
   <div class="flex_wrap">
     <div class="flex_img">
-      <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kiyou/detail02.jpg">
+      <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kyohou/detail02.jpg">
     </div>
     <div class="flex_txt">
       <div class="comp-title">
@@ -33,18 +33,18 @@
         <span class="ttl_en">FLAVOR</span>
       </div>
       <div class="sp_img">
-        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kiyou/detail02.jpg">
+        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kyohou/detail02.jpg">
       </div>
       <p class="txt">優しく続く甘みと、あとから感じる巨峰の香りがくせになる。<span>何個でも食べたくなる味わいです。</span></p>
       <div class="chart_wrap">
-        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kiyou/chart.png">
+        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kyohou/chart.png">
       </div>
     </div>
   </div><!-- flex_wrap -->
   <div class="flex_origin">
     <div class="flex_img">
       <div class="origin_img">
-        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kiyou/location.png">
+        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kyohou/location.png">
       </div>
     </div>
     <div class="flex_txt">
@@ -53,7 +53,7 @@
         <span class="ttl_en">PLACE OF ORIGIN</span>
       </div>
       <div class="sp_img">
-        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kiyou/location.png">
+        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kyohou/location.png">
       </div>
       <div class="origin_upper">
         <div class="origin_item">
--- color-me-files/edited/甲州百目.php
+++ color-me-files/edited/甲州百目.php
@@ -6,7 +6,7 @@
   <div class="inner">
   <div class="flex_wrap reverse">
     <div class="flex_img">
-      <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kiyou/detail01.jpg">
+      <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/hyakume/detail01.jpg">
     </div>
     <div class="flex_txt">
       <div class="comp-title">
@@ -14,7 +14,7 @@
         <span class="ttl_en">FEATURES</span>
       </div>
       <div class="sp_img">
-        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kiyou/detail01.jpg">
+        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/hyakume/detail01.jpg">
       </div>
       <!--<h3 class="copy">
         <span>収穫期間、わずか10日。</span>
@@ -25,7 +25,7 @@
   </div><!-- flex_wrap -->
   <div class="flex_wrap">
     <div class="flex_img">
-      <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kiyou/detail02.jpg">
+      <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/hyakume/detail02.jpg">
     </div>
     <div class="flex_txt">
       <div class="comp-title">
@@ -33,18 +33,18 @@
         <span class="ttl_en">FLAVOR</span>
       </div>
       <div class="sp_img">
-        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kiyou/detail02.jpg">
+        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/hyakume/detail02.jpg">
       </div>
       <p class="txt">1つ1つ丁寧に手作業でブロック状にカットして乾かしました。<span>口いっぱいに広がる柿の風味と、まるで焼き芋のようなねっとりした食感をお楽しみいただけます。</span></p>
       <div class="chart_wrap">
-        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kiyou/chart.png">
+        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/hyakume/chart.png">
       </div>
     </div>
   </div><!-- flex_wrap -->
   <div class="flex_origin">
     <div class="flex_img">
       <div class="origin_img">
-        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kiyou/location.png">
+        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/hyakume/location.png">
       </div>
     </div>
     <div class="flex_txt">
@@ -53,7 +53,7 @@
         <span class="ttl_en">PLACE OF ORIGIN</span>
       </div>
       <div class="sp_img">
-        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/kiyou/location.png">
+        <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/product_detail/hyakume/location.png">
       </div>
       <div class="origin_upper">
         <div class="origin_item">
