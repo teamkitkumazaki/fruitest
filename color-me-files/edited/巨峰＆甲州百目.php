6ac023efb807bb21eab3b8d19b298b4de06ba752
Kumazaki Shinnosuke <s.kumazaki@mikke.co.jp>
2019-06-12 16:42:46 +0900

    セット商品に関するレイアウトを構築

---
+++ color-me-files/edited/巨峰＆甲州百目.php
@@ -0,0 +1,99 @@
+<div id="leadTxt">
+  <div class="set_content">
+    <div class="set_detail">
+      <span>巨峰</span>
+      <span>甲州百目</span>
+    </div>
+  </div>
+</div>
+
+<section id="setContent">
+  <div class="inner">
+    <div class="comp-title">
+      <h2 class="ttl_ja">セット内容</h2>
+      <span class="ttl_en">CONTENTS</span>
+    </div>
+  <div class="comp-setcontent">
+    <div class="img_wrap">
+      <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/set_product/pione_hyakume/setimg01.jpg">
+    </div>
+    <div class="txt_wrap">
+      <div class="comp-title">
+        <h2 class="ttl_ja">巨峰</h2>
+        <span class="ttl_en">KYOHOU</span>
+      </div>
+      <div class="detail">
+        <h3 class="detail_ttl">
+          <span>半世紀たっても色あせない。</span>
+          <span>ぶどう農家が愛するぶどう。</span>
+        </h3>
+        <p class="detail_txt">
+          <span>フルーツ王国山梨県のぶどう農家が口をそろえて「一番好き」と言う、元祖プレミアムぶどうの巨峰。</span>
+          <span>品種登録から半世紀以上経っても未だにその味と人気は色あせません。</span>
+          <span>特にFRUITESTが使っている、山梨市牧岡地区の昔ながらの種あり巨峰の味は別格。適度な甘みと、味の輪郭を際立たせる酸味のバランスを大切にして仕上げました。</span>
+        </p>
+      </div>
+      <div class="comp-linkbutton">
+        <a href="/?pid=142903040"><span>詳しく知る<font>READ MORE</font></span></a>
+      </div>
+    </div>
+  </div><!-- comp-setcontent -->
+  <div class="comp-setcontent reverse">
+    <div class="img_wrap">
+      <img src="https://journal.fruitest.jp/wp-content/themes/fruitest-theme/img/set_product/pione_hyakume/setimg02.jpg">
+    </div>
+    <div class="txt_wrap">
+      <div class="comp-title">
+        <h2 class="ttl_ja">甲州百目</h2>
+        <span class="ttl_en">KOSYUHYAKUME</span>
+      </div>
+      <div class="detail">
+        <h3 class="detail_ttl">
+          <span>新しく、どこか懐かしい。</span>
+          <span>一度食べたらとまらない、優しさの味。</span>
+        </h3>
+        <p class="detail_txt">
+          <span>山梨では古くから干し柿やあんぽ柿にして親しまれてきた甲州百目。その中でも一級品の完熟柿だけを厳選し、レア・ドライにしました。</span><span>生柿のようでいながらもねっとりと柔らかい食感と、ほんのり続く優しい甘みがクセになります。新しいのにどこか懐かしい味わいを、ご堪能ください。</span></p>
+      </div>
+      <div class="comp-linkbutton">
+        <a href="/?pid=142905939"><span>詳しく知る<font>READ MORE</font></span></a>
+      </div>
+    </div>
+  </div><!-- comp-setcontent -->
+</div><!-- inner -->
+</section><!-- setContent -->
+<section id="itemInfo">
+  <div class="inner">
+    <div class="comp-title block">
+      <h2 class="ttl_ja">商品情報</h2>
+      <span class="ttl_en">ITEM DETAIL</span>
+    </div>
+    <div class="info_wrap">
+      <div class="info_item block">
+        <span class="title">原材料</span>
+        <span class="content">巨峰：ぶどう（巨峰）<br>甲州百目：柿（甲州百目）</span>
+      </div>
+      <div class="info_item block">
+        <span class="title">産地</span>
+        <span class="content">巨峰：山梨県山梨市（牧岡地区）<br> 甲州百目：山梨県甲州市</span>
+      </div>
+      <div class="info_item block">
+        <span class="title">内容量</span>
+        <span class="content">巨峰：12個（12粒）<br>甲州百目：7個（7粒）</span>
+      </div>
+      <div class="info_item block">
+        <span class="title">保存方法</span>
+        <span class="content">
+          <span class="content_inner">
+            <font>巨峰</font>
+						※直射日光を避け、冷暗所に保存してください<br>※賞味期限は製造から1年になります。商品の缶底をご確認ください。<br>※手作業で種抜きをしていますが、まれに種が残っている場合があります。<br>※開封後は密封し、お早めにお召し上がりください。<br>※開封後の保存は、冷蔵庫をおすすめします。<br>※商品の仕様などは予告なく変更になる場合があります。予めご了承ください。
+          </span>
+          <span class="content_inner">
+						<font>甲州百目</font>
+						※直射日光を避け、冷暗所に保存してください<br>※賞味期限は製造から1年になります。商品の缶底をご確認ください。<br>※開封後は密封し、お早めにお召し上がりください。<br>※開封後の保存は、冷蔵庫をおすすめします。<br>※商品の仕様などは予告なく変更になる場合があります。予めご了承ください。
+          </span>
+        </span><!-- content -->
+      </div>
+    </div><!-- info_wrap -->
+  </div><!-- inner-->
+</section><!-- itemInfo -->
