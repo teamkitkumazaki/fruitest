4c08a9ca6d95805fc23682ae4274cb4397758f38
Kumazaki Shinnosuke <s.kumazaki@mikke.co.jp>
2019-06-16 20:42:38 +0900

    商品一覧ページのフィードバック修正対応

--- color-me-files/edited/商品一覧.php
+++ color-me-files/edited/商品一覧.php
@@ -34,6 +34,18 @@
             <{/if}>
             <div class="img_wrap">
               <a href="<{$productlist[num].link_url}>" style="background-image: url(<{$productlist[num].img_url}>)"></a>
+              <{if $productlist[num].soldout_flg == false}>
+                <{if $productlist[num].teika_disp == true}>
+                <!-- 通常 -->
+                <span class="stock">販売中</span>
+                <{else}>
+                <!-- Upcoming -->
+                <span class="upcoming">構想中</span>
+                <{/if}>
+                <{else}>
+                  <!-- 売り切れ -->
+                  <span class="soldout">再販受付中</span>
+                <{/if}>
           </div>
           <div class="txt_wrap">
             <a class="product_name" href="<{$productlist[num].link_url}>"><{$productlist[num].name}><span><{$productlist[num].model}></span></a>
@@ -72,35 +84,24 @@
   </div><!-- inner -->
 </section><!-- productList -->
 <section class="comp-option">
-  <div class="comp-title center">
-    <h2 class="ttl_ja">購入オプション</h2>
-    <span class="ttl_en">OPTIONS</span>
-  </div>
-  <div class="lead_txt">
-    <p><span>FRUITESTでは、記念日の贈り物向けのギフトセット・ラッピングサービスや</span><span>季節毎に旬の果物をお届けする定期便など、様々なオプションをご用意しております。</span></p>
-  </div>
-  <div class="flex_wrap">
-    <div class="block gift">
-      <span class="sp_ttl">ギフト商品一覧</span>
-      <a class="img_wrap" href="/?mode=f2"></a>
-      <div class="txt_wrap">
-        <span class="ttl">ギフト商品一覧</span>
-        <div class="comp-linkbutton-mini">
-          <a href="/?mode=f2"><span>詳しく知る</span></a>
-        </div>
-      </div>
+  <div class="inner">
+    <div class="comp-title center">
+      <h2 class="ttl_ja">FRUITESTのギフトセット</h2>
+      <span class="ttl_en">GIFT SET</span>
     </div>
-    <div class="block subscription">
-      <span class="sp_ttl">定期便について</span>
-      <a class="img_wrap" href="#aaa"></a>
-      <div class="txt_wrap">
-        <span class="ttl">定期便について</span>
-        <div class="comp-linkbutton-mini">
-          <a href="#aaaa"><span>詳しく知る</span></a>
-        </div>
-      </div>
+    <div class="spimg_wrap"></div>
+    <div class="txt_wrap">
+      <p class="text">
+        <span>手土産や特別な日のプレゼントに。</span>
+        <span>旬のレア・ドライフルーツを、ギフトボックスに入れてお届けします。</span>
+      </p>
     </div>
-  </div>
+    <div class="img_wrap"></div>
+    <div class="comp-linkbutton">
+      <a href="/?mode=f2">
+        <span>ギフトセット一覧<font>GIFT SET</font></span></a>
+    </div>
+  </div><!-- inner -->
 </section><!-- topOption -->
 </article>
 
